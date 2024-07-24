fd746a33999962ad2a7aa8c7da6a315e6ed162ed
Gourav Sharma <gouravsharma2806@gmail.com>
2024-07-24 11:49:30 +0530

    Connected Monogo with first way

---
+++ Day 4/src/index.js
@@ -0,0 +1,24 @@
+import { mongoose } from "mongoose";
+import { DB_NAME } from "./constrants";
+
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URl}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("error");
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Port is listen on ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+// function connectDB() {}
+// connectDB();
