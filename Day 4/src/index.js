404a45940c7df499f77291fe3ebf82534040648e
Gourav Sharma <gouravsharma2806@gmail.com>
2024-07-24 14:50:56 +0530

    custom api response

--- Day 4/src/index.js
+++ Day 4/src/index.js
@@ -6,7 +6,15 @@ import connectDB from "./db/index.js";
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server Started");
+    });
+  })
+  .catch((error) => {
+    console.log("DB connection failed", error);
+  });
 
 /*
 import express from "express";
